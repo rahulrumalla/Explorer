3234cddac103492a7e5bb9084d3dfd1a07d03c3f
Matt Garnett <matt@mattgarnett.com>
2018-10-11 11:58:06 +0200

    add ability to require src in text component to be represented as an absolute link instead of relative

--- src/containers/Bounty/index.js
+++ src/containers/Bounty/index.js
@@ -341,6 +341,7 @@ class BountyComponent extends React.Component {
                       </i>
                       <Text
                         link
+                        absolute
                         src={`https://ipfs.infura.io/ipfs/${
                           bounty.sourceDirectoryHash
                         }/${bounty.sourceFileName}`}
@@ -355,7 +356,7 @@ class BountyComponent extends React.Component {
                       <i className={styles.metadataIcon}>
                         <FontAwesomeIcon icon={['far', 'link']} />
                       </i>
-                      <Text link src={`${bounty.webReferenceURL}`}>
+                      <Text link absolute src={`${bounty.webReferenceURL}`}>
                         {shortenUrl(bounty.webReferenceURL)}
                       </Text>
                     </div>
